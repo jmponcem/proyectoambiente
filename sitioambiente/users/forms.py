1f89bc741c1203187d8605befbcec44532e78f8a
jose <josemiguelponcem@gmail.com>
2019-11-05 22:44:45 -0300

    Proyecto ambiente, login y registro extendido

---
+++ sitioambiente/users/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+
+# Extendemos del original
+
+class RegisterForm(UserCreationForm):
+    # Establecemos que el campo username es tipo email y el nombre
+    
+    email = forms.EmailField(label="Correo electrónico")
+    first_name = forms.CharField(label="Nombre")
+    last_name = forms.CharField(label="Apellido")
+    
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2", "first_name", "last_name"]
